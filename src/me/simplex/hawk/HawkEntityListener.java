2ba7a458fbb80b76f5ce1d8017908502ec748b2a
s1mpl3x <simplex21@googlemail.com>
2012-02-22 04:33:15 +0100

    relase commit

--- src/me/simplex/hawk/HawkEntityListener.java
+++ src/me/simplex/hawk/HawkEntityListener.java
@@ -7,13 +7,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
-public class HawkEntityListener implements Listener{
+public class HawkEntityListener implements Listener {
 	private Hawk main;
-	
+
 	public HawkEntityListener(Hawk main) {
 		this.main = main;
 	}
-		
+
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerDmg(EntityDamageEvent event) {
 		if (event.getEntity() instanceof Player) {
@@ -21,15 +21,14 @@ public class HawkEntityListener implements Listener{
 			if (main.flyingPlayers.containsKey(player.getName())) {
 				if (event.getCause().equals(DamageCause.FALL)) {
 					event.setCancelled(true);
-					player.sendMessage("[CASE 1] Fall dmg event cancelled");
+					// player.sendMessage("[CASE 1] Fall dmg event cancelled");
 				}
-			}
-			else if (main.dmgImunePlayers.contains(player.getName())) {
+			} else if (main.dmgImunePlayers.contains(player.getName())) {
 				event.setCancelled(true);
-				player.sendMessage("[CASE 2] Fall dmg event cancelled");
-				main.toLogger("List Length: " + main.dmgImunePlayers.size());
+				// player.sendMessage("[CASE 2] Fall dmg event cancelled");
+				// main.toLogger("List Length: " + main.dmgImunePlayers.size());
 				main.dmgImunePlayers.remove(player.getName());
-				main.toLogger("List Length: " + main.dmgImunePlayers.size());
+				// main.toLogger("List Length: " + main.dmgImunePlayers.size());
 			}
 		}
 	}
